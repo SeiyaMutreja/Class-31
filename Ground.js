7c0aa9b90277a18b8d5ffc6437377d171ad864ed
SeiyaMutreja <seiyamutreja@gmail.com>
2021-01-03 17:43:00 +0530

    smoke trajectory

--- Ground.js
+++ Ground.js
@@ -11,7 +11,7 @@ class Ground {
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill("brown");
+      fill(48,22,8);
       rect(pos.x, pos.y, this.width, this.height);
     }
   };
